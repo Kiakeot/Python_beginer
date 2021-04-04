10b1e9b34e44d80b2eb3adf701d33dd3a3a94fe5
Kiakeot <solievnikita@gmail.com>
2021-04-04 13:07:49 +0300

    Update 2.py
    change

--- Lesson5/2.py
+++ Lesson5/2.py
@@ -1,5 +1,10 @@
 d = {"a":500, 'b':5874, "c":560, "d":400, "e":5874, "f":20}
-inverse = [(value, key) for key, value in d.items()]
-print max(inverse)[1]
-print max(inverse)[2]
-print max(inverse)[3]
+max1 = max(d).
+print(max1)
+remove(max1)
+max2 = max(d).
+print(max2)
+remove(max2)
+max3 = max(d).
+print(max3)
+remove(max3)
