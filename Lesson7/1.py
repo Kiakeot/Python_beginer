d0b3a4b9e9b4591ef27380f1652027cd48a5b017
Kiakeot <solievnikita@gmail.com>
2021-04-18 16:55:26 +0300

    done

---
+++ Lesson7/1.py
@@ -0,0 +1,13 @@
+input("a and b")
+znak = input("знак")
+def arithmetic(a,b,znak):
+	if znak == +:
+		print(a+b)
+	elif znak == -:
+		print(a-b)
+	elif znak == *:
+		print(a*b)
+	elif znak == /:
+		print(a/b)
+	elif : 
+		print("Неизвестная операция")
---
+++ Lesson7/2.py
@@ -0,0 +1,6 @@
+god = input("Введите год")
+def is_year_leap(god):
+	if god % 4 == 0:
+		prinr(True)
+	elif :
+		print(False) 
